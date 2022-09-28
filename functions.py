2bcd7f624088f05072baab7175f18a5c69d749e1
夢遊者 <71359260+Lunatik-cyber@users.noreply.github.com>
2022-09-29 01:23:06 +0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,11 +1,8 @@
 import os
 
-import requests
 from icq import bot
-from requests import Request
 
 temp_dir = os.path.join(os.getcwd(), 'temp')
-api_base_url = "https://api.icq.net/bot/v1"
 
 
 class ICQ_transfer:
@@ -13,7 +10,6 @@ class ICQ_transfer:
         self.user_id = user_id
 
     def send_file_to_icq(self, file_name):
-        # Отправка файла
         file_path = os.path.join(temp_dir, file_name)
         with open(file_path, 'rb') as file:
             bot.send_file(chat_id=self.user_id, file=file, file_name=file_name)
